a7433337f80072467ec91a7cee0a3eee853087e0
Ryan McLaughlin <mclaughlinr2@gmail.com>
2023-05-21 15:42:00 +0000

    emb plot

--- dev_utils/plot_emb.py
+++ dev_utils/plot_emb.py
@@ -91,24 +91,15 @@ emb_df.drop(columns=['y_labels'], inplace=True)
 reducer = umap.UMAP(min_dist=0.1, n_neighbors=15, metric='manhattan', 
 					n_components=2, random_state=42
 					)
+
+###############################################################################################
+# use labels to fit
+###############################################################################################
 umap_fit = reducer.fit(anchor_df.values, anchor_labels)
 umap_emb = umap_fit.transform(emb_df.values)
-#umap_emb = reducer.fit_transform(emb_df.values)
 umap_df = pd.DataFrame(umap_emb, index=emb_df.index).reset_index()
 umap_df['contig_id'] = [x.rsplit('_', 1)[0] for x in umap_df['subcontig_id']]
 
-'''
-recruited_list = []
-for fav in fav_list:
-	sub_clust_df = clust_df.query("best_label == @fav")
-	sub_umap_df = umap_df.merge(sub_clust_df, on='contig_id')
-	sub_umap_df['type'] = sub_umap_df['best_label']
-	sub_umap_df['stage'] = ['anchor' if x in list(mhr_df['q_contig_id'])
-							 else 'cluster' for x in sub_umap_df['contig_id']
-							 ]
-	recruited_list.append(sub_umap_df)
-recruited_df = pd.concat(recruited_list)
-'''
 recruited_df = umap_df.merge(clust_df, on='contig_id')
 recruited_df['type'] = 'recruited'
 recruited_df['stage'] = ['anchor' if x in best_anchors
@@ -217,3 +208,120 @@ with PdfPages("/home/ryan/SABer_local/SI/QC_run/xPGs/genus_cluster_umap.pdf") as
 			pdf_pages.savefig(figu)
 			plt.clf()
 			plt.close()
+
+###############################################################################################
+# no labels for fitting
+###############################################################################################
+umap_nolabel = reducer.fit_transform(emb_df.values)
+umap_nl_df = pd.DataFrame(umap_nolabel, index=emb_df.index).reset_index()
+umap_nl_df['contig_id'] = [x.rsplit('_', 1)[0] for x in umap_nl_df['subcontig_id']]
+
+recruited_df = umap_nl_df.merge(clust_df, on='contig_id')
+recruited_df['type'] = 'recruited'
+recruited_df['stage'] = ['anchor' if x in best_anchors
+						 else 'not_recruited' for x in recruited_df['contig_id']
+						 ]
+recruited_df['stage'] = ['cluster' if x in best_clusters
+						 else y for x,y in 
+						 zip(recruited_df['contig_id'], recruited_df['stage'])
+						 ]
+
+recruit_list = list(recruited_df['contig_id'].unique())
+unrecruited_df = umap_nl_df.query("contig_id not in @recruit_list")
+unrecruited_df['type'] = 'not_recruited'
+unrecruited_df['stage'] = 'not_recruited'
+
+cat_df = pd.concat([recruited_df[['subcontig_id', 0, 1, 'type', 'stage']],
+					unrecruited_df[['subcontig_id', 0, 1, 'type', 'stage']]]
+					)
+genome_id = []
+for sc in list(cat_df['subcontig_id']):
+	c_id = sc.rsplit('_', 1)[0]
+	if c_id in list(mhr2sag.keys()):
+		g_id = mhr2sag[c_id]
+	elif c_id in list(clust2sag.keys()):
+		g_id = clust2sag[c_id]
+	else:
+		g_id = None
+
+	genome_id.append(g_id)
+cat_df['Genome_Id'] = genome_id
+cat_df['family'] = [sag2fam[s] if s in list(sag2fam.keys())
+					else None for s in cat_df['Genome_Id']
+					]
+cat_df['genus'] = [sag2gen[s] if s in list(sag2fam.keys())
+					else None for s in cat_df['Genome_Id']
+					]
+'''
+sctr = sns.scatterplot(data=cat_df.query("type == 'not_recruited'"),
+					   x=0, y=1, hue='type', palette=['gray'],
+					   alpha=0.50, marker='.', linewidth=0.1, s=10,
+					   legend=False
+					   )
+sctr = sns.scatterplot(data=cat_df.query("type != 'not_recruited'"),
+					   x=0, y=1, hue='family', alpha=0.50,
+					   linewidth=0.1, markers='o', s=10,
+					   )
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+#plt.legend('', frameon=False)
+
+sctr.figure.savefig("/home/ryan/SABer_local/SI/QC_run/xPGs/family_cluster_umap.pdf", dpi=300, bbox_inches='tight')
+plt.clf()
+plt.close()
+'''
+family_cmap = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink']
+genus_cmap = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink']
+family_list = list(cat_df['family'].unique())
+genus_list = list(cat_df['genus'].unique())
+with PdfPages("/home/ryan/SABer_local/SI/QC_run/xPGs/family_cluster_umap_nolabel.pdf") as pdf_pages:
+	for i,f_id in enumerate(family_list):
+		if f_id != None:
+			print(f_id)
+			f_pal = family_cmap[i]
+			fam_cat_df = cat_df.query("family == @f_id")
+			non_cat_df = cat_df.query("family != @f_id | type == 'not_recruited'")
+			non_cat_df['type'] = 'not_recruited'
+			print(fam_cat_df.head())
+			print(non_cat_df.head())
+			figu = plt.figure()
+			sctr = sns.scatterplot(data=non_cat_df, x=0, y=1, hue='type', palette=['gray'],
+								   alpha=0.50, marker='.', linewidth=0.1, s=10,
+								   legend=False
+								   )
+			sctr = sns.scatterplot(data=fam_cat_df, x=0, y=1, hue='family', palette=[f_pal],
+								   alpha=0.50, linewidth=0.1, markers='o', s=10,
+								   legend=False, 
+								   ).set_title(f_id)
+			#plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+			#plt.legend('', frameon=False)
+
+			pdf_pages.savefig(figu)
+			plt.clf()
+			plt.close()
+
+with PdfPages("/home/ryan/SABer_local/SI/QC_run/xPGs/genus_cluster_umap_nolabel.pdf") as pdf_pages:
+	for i,g_id in enumerate(genus_list):
+		if g_id != None:
+			print(f_id)
+			g_pal = genus_cmap[i]
+			gen_cat_df = cat_df.query("genus == @g_id")
+			non_cat_df = cat_df.query("genus != @g_id | type == 'not_recruited'")
+			non_cat_df['type'] = 'not_recruited'
+			print(gen_cat_df.head())
+			print(non_cat_df.head())
+			figu = plt.figure()
+			sctr = sns.scatterplot(data=non_cat_df, x=0, y=1, hue='type', palette=['gray'],
+								   alpha=0.50, marker='.', linewidth=0.1, s=10,
+								   legend=False
+								   )
+			sctr = sns.scatterplot(data=gen_cat_df, x=0, y=1, hue='genus', palette=[g_pal],
+								   alpha=0.50, linewidth=0.1, markers='o', s=10,
+								   legend=False, 
+								   ).set_title(g_id)
+			#plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
+			#plt.legend('', frameon=False)
+
+			pdf_pages.savefig(figu)
+			plt.clf()
+			plt.close()
+
